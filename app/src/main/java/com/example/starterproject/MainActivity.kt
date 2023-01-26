9d00e9a539fa57b07c6019162902b40130d959af
Amaan Rajguru <99455992+Amrajguru@users.noreply.github.com>
2023-01-26 16:32:57 -0500

    Lab1 Complete

--- app/src/main/java/com/example/starterproject/MainActivity.kt
+++ app/src/main/java/com/example/starterproject/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.starterproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter = 0;
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.Valuenumber)
+        textView.text = counter.toString()
+
+        button.setOnClickListener{
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++;
+            textView.text = counter.toString()
+        }
     }
 }
